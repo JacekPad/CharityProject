0d83861e535127805d3490af80e5cab3e994a306
JacekPad <jacekpadyjasek@gmail.com>
2022-05-09 02:06:40 +0200

    Password regex and length

--- src/main/java/pl/coderslab/security/UserServiceImpl.java
+++ src/main/java/pl/coderslab/security/UserServiceImpl.java
@@ -8,7 +8,6 @@ import pl.coderslab.user.UserRepository;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
